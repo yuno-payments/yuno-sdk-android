7821e406fdec7e8ef9fb46124c1a3723ad222805
Andrés Páez <andres@y.uno>
2022-09-14 10:37:23 -0300

    Project | Remove Test Api key

--- app/src/main/java/com/yuno/payments/example/CustomApplication.kt
+++ app/src/main/java/com/yuno/payments/example/CustomApplication.kt
@@ -6,6 +6,6 @@ import com.yuno.payments.core.Yuno
 class CustomApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Yuno.initialize(this, "prod_gAAAAABiq1BM0un867cfl2lDn2vJHYVQodf2oQgJSX8UGzsoQhz1PtgV4hYFc-LGqt3Ho0BfaN3vMH07G9g-2H3kLGIUrGd6cO2mONeXVV13FoN9Em_k52wcr21366caJ1fX9q-zJCc1lghemfFewuDAmY1Z0ZW3ALLEhGPlRHZnpp4WvqjcpKs=")
+        Yuno.initialize(this, "API_KEY")
     }
 }
