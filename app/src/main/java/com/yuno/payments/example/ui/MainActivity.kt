ca1127ebd38f4af42c1f8917ee55d0a747185f04
Sebastian Garcia <sebastian.garcia@y.uno>
2022-11-01 11:15:56 -0500

    Project | Re design MainActivity.kt (#15)

--- app/src/main/java/com/yuno/payments/example/ui/MainActivity.kt
+++ app/src/main/java/com/yuno/payments/example/ui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.yuno.payments.example.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.yuno.payments.base.extensions.style
 import com.yuno.payments.example.ui.payment.CheckoutLiteActivity
 import com.yuno.payments.core.Yuno
 import com.yuno.payments.example.R
@@ -16,33 +19,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setListeners()
+        initListeners()
+        findViewById<TextView>(R.id.textView_secure_payment).text =
+            SpannableStringBuilder().style(this, R.style.TextSmall_LowTextGrey) {
+                append(getString(R.string.secure_payment))
+            }.style(this, R.style.TextSmall_PurpleLight_Bold) {
+                append(" YUNO")
+            }
     }
 
-    private fun setListeners() {
-        findViewById<Button>(R.id.button_change_api_key).setOnClickListener {
-            Yuno.initialize(
-                this.application,
-                findViewById<EditText>(R.id.editText_api_key).text.toString()
-            )
+    private fun initListeners() {
+        findViewById<Button>(R.id.button_add_payment).setOnClickListener {
+            startActivity(Intent(this, EnrollPaymentActivity::class.java))
         }
 
-        findViewById<Button>(R.id.button_start_enrollment_flow).setOnClickListener {
-            startActivity(
-                Intent(this, EnrollPaymentActivity::class.java)
-            )
-        }
+        findViewById<Button>(R.id.button_payment_lite)
+            .setOnClickListener {
+                startActivity(Intent(this, CheckoutLiteActivity::class.java))
+            }
 
-        findViewById<Button>(R.id.button_payment_lite).setOnClickListener {
-            startActivity(
-                Intent(this, CheckoutLiteActivity::class.java)
-            )
-        }
-
-        findViewById<Button>(R.id.button_payment_complete).setOnClickListener {
-            startActivity(
-                Intent(this, CheckoutCompleteActivity::class.java)
-            )
-        }
+        findViewById<Button>(R.id.button_payment_complete)
+            .setOnClickListener {
+                startActivity(Intent(this, CheckoutCompleteActivity::class.java))
+            }
     }
 }
