ab53a7ce71fc4a7d4b0bf6ede03651b38af4a2ee
Vladimir <vladimir.fernandez@y.uno>
2023-01-10 15:56:03 -0500

    Project | Update SDK Version to 1.3.0

--- app/src/main/java/com/yuno/payments/example/CustomApplication.kt
+++ app/src/main/java/com/yuno/payments/example/CustomApplication.kt
@@ -2,10 +2,15 @@ package com.yuno.payments.example
 
 import android.app.Application
 import com.yuno.payments.core.Yuno
+import com.yuno.payments.core.YunoConfig
+import com.yuno.payments.features.base.ui.screens.CardFormType
 
 class CustomApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Yuno.initialize(this, "API_KEY")
+        Yuno.initialize(this, "API_KEY", config = YunoConfig(
+            cardFlow = CardFormType.MULTI_STEP,
+            saveCardEnabled = true
+        ))
     }
 }
